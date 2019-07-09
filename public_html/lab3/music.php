450b6d1b5ff82db87873451aae1a238d2b1a14e2
Angarag <mars0411@gmail.com>
2019-07-09 09:10:26 -0500

    Lab3 ongoing

---
+++ public_html/lab3/music.php
@@ -0,0 +1,123 @@
+
+
+<!DOCTYPE html>
+<html>
+	<!--
+	Web Programming Step by Step
+	Lab #3, PHP
+	-->
+
+	<head>
+		<title>Music Viewer</title>
+		<meta charset="utf-8" />
+		<link href="viewer.css" type="text/css" rel="stylesheet" />
+	</head>
+
+	<body>
+        <?php
+		$num_songs=5678;
+		function getMP3Files(){
+			$ul= "<ul id=\"musiclist\">";
+			$li="<li class=\"mp3item\">";
+			$ul_end="</ul>";
+			$li_end="</li>";
+			echo $ul;
+			echo $li;
+			echo $li_end;
+			echo $ul_end;
+		}
+        function getLinks(){
+			$num_links=$_GET["newspages"]!=NULL?$_GET["newspages"]:5;
+            echo "<ol>";
+            for($i=1;$i<=$num_links;$i++){
+                echo "<li><a href='http://music.yahoo.com/news/archive/".$i.".html'>Page ".$i."</a></li>";
+            }
+            echo "</ol>";
+		}
+		
+		function getFavoriteArtists(){
+			$artists=array("Britney Spears","Christina Aguilera","Justin Bieber");
+			$artists=file("./favorite.txt");
+			echo "<ol>";
+			foreach($artists as $artist)
+				echo "<li>".makeArtistNameLink($artist)."</li>";
+			echo "</ol>";
+		}
+		function makeArtistNameLink($name){
+			$result= "<a href=\"http://music.yahoo.com/videos/".formatName($name)."\">".$name."</a>";
+			//echo $result;
+			return $result;
+		}
+		function formatName($name){
+			$name=strtolower($name);
+			$name=str_replace(" ","-",$name);
+			return $name;
+		}
+
+        ?>
+		<h1>My Music Page</h1>
+		
+		<!-- Exercise 1: Number of Songs (Variables) -->
+		<p>
+			I love music.
+			I have <?=$num_songs?> total songs,
+			which is over <?=(float)$num_songs/10?> hours of music!
+		</p>
+
+		<!-- Exercise 2: Top Music News (Loops) -->
+		<!-- Exercise 3: Query Variable -->
+		<div class="section">
+			<h2>Yahoo! Top Music News</h2>
+           <?php getLinks();?>
+		</div>
+
+		<!-- Exercise 4: Favorite Artists (Arrays) -->
+		<!-- Exercise 5: Favorite Artists from a File (Files) -->
+		<div class="section">
+			<h2>My Favorite Artists</h2>
+			<?php getFavoriteArtists();?>
+		</div>
+		
+		<!-- Exercise 6: Music (Multiple Files) -->
+		<!-- Exercise 7: MP3 Formatting -->
+		<div class="section">
+			<h2>My Music and Playlists</h2>
+			<?=getMP3Files();?>
+<!-- 
+			<ul id="musiclist">
+				<li class="mp3item">
+					<a href="http://mumstudents.org/cs472/Labs/3/songs/be-more.mp3">be-more.mp3</a>
+				</li>
+				
+				<li class="mp3item">
+					<a href="http://mumstudents.org/cs472/Labs/3/songs/just-because.mp3">just-because.mp3</a>
+				</li>
+
+				<li class="mp3item">
+					<a href="http://mumstudents.org/cs472/Labs/3/songs/drift-away.mp3">drift-away.mp3</a>
+				</li>
+
+				<!-- Exercise 8: Playlists (Files) -->
+				<!-- <li class="playlistitem">472-mix.m3u:
+					<ul>
+						<li>Hello.mp3</li>
+						<li>Be More.mp3</li>
+						<li>Drift Away.mp3</li>
+						<li>190M Rap.mp3</li>
+						<li>Panda Sneeze.mp3</li>
+					</ul>
+				</li>
+			</ul> --> 
+		</div>
+
+		<div>
+			<a href="http://validator.w3.org/check/referer">
+				<img src="http://mumstudents.org/cs472/Labs/3/w3c-html.png" alt="Valid HTML5" />
+			</a>
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">
+				<img src="http://mumstudents.org/cs472/Labs/3/w3c-css.png" alt="Valid CSS" />
+			</a>
+		</div>
+	</body>
+</html>
+
